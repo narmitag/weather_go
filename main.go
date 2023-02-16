69bccba9e17e0d2cb1add3369a2864cbc13fcfe9
Neil Armitage <neil@neila.co.uk>
2023-02-16 15:54:34 +0000

    remove hard coded port

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,11 +12,13 @@ func main() {
 
 	flag.Parse()
 
+	httpPort := os.Getenv("PORT")
+
 	if *httpPtr {
-		println("Listening for data on :8081")
+		println("Listening for data on :" + httpPort)
 		http.HandleFunc("/", DataHandler(*dataPath))
 		http.HandleFunc("/health", alive)
-		http.ListenAndServe(":8081", nil)
+		http.ListenAndServe(":"+httpPort, nil)
 	} else {
 		ExtractData(*dataPath)
 	}
